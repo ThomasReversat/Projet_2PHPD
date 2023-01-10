16c4636ee66f2e429641f748b2271773b5af3c1c
KWS0526 <yangv140@gmail.com>
2023-01-10 09:20:44 +0100

    ajout

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <title>Internet Movies DataBase & co</title>
     <link rel="shortcut icon" type="image/ico" href="img/icone.ico">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/default-style.css">
+
 </head>
 <body>
 <div class="header">
@@ -21,31 +23,35 @@
     <button type="submit">Go</button>
 </form>
 
-<div class="main-content test">
+<div class="container">
 
-    <div class="movie">
-        <img src="img/titanic.jpg" alt="Movie Poster">
-        <h3>Titanic</h3>
+  <div class="image-block">
+    <img src="img/titanic.jpg" alt="Image 1">
+    <h3>Titanic</h3>
         <p>En 1997, l'épave du Titanic est l'objet d'une exploration fiévreuse, menée par des chercheurs de trésor
             en quête d'un diamant bleu qui se trouvait à bord. Frappée par un reportage télévisé, l'une des rescapées
             du naufrage, âgée de 102 ans, Rose DeWitt, se rend sur place et évoque ses souvenirs. 1912.</p>
-    </div>
-    <div class="movie">
-        <img src="img/avatar.jpg" alt="Movie Poster">
+  </div>
+
+  <div class="image-block">
+    <img src="img/avatar.jpg" alt="Movie Poster">
         <h3>Avatar: La voie de l'eau</h3>
         <p>Jake Sully et Ney'tiri ont formé une famille et font tout pour rester aussi soudés que possible.
             Ils sont cependant contraints de quitter leur foyer et d'explorer les différentes régions encore mystérieuses
             de Pandora. Lorsqu'une ancienne menace refait surface, Jake va devoir mener une guerre difficile contre les
             humains.
         </p>
-    </div>
-    <div class="movie">
-        <img src="img/topgun.jpg" alt="Movie Poster">
+  </div>
+
+  <div class="image-block">
+    <img src="img/topgun.jpg" alt="Movie Poster">
         <h3>Top Gun: Maverick</h3>
         <p>Après plus de 30 ans de service en tant que l'un des meilleurs aviateurs de la Marine,
             Pete Maverick Mitchell est à sa place, repoussant les limites en tant que pilote d'essai courageux et
             esquivant l'avancement de grade qui le mettrait à la terre.</p>
-    </div>
+        </p>
+  </div>
 </div>
+
 </body>
 </html>
