4d2f05f640d60aded8a8e59a40cf069c447b2319
KWS0526 <yangv140@gmail.com>
2023-01-11 00:49:34 +0100

    page connexion

--- accueil.php
+++ accueil.php
@@ -3,7 +3,6 @@
 <head>
     <title>Internet Movies DataBase & co</title>
     <link rel="shortcut icon" type="image/ico" href="img/icone.ico">
-    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/accueil.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <meta charset="utf-8">
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php 
+   session_start() ;
+  if(isset($_POST['boutton-valider'])){ 
+    if(isset($_POST['email']) && isset($_POST['mdp'])) {
+        $email = $_POST['email'] ;
+        $mdp = $_POST['mdp'] ;
+        $erreur = "" ;
+        $nom_serveur = "localhost";
+        $utilisateur = "root";
+        $mot_de_passe ="";
+        $nom_base_données ="projet_php" ;
+        $con = mysqli_connect($nom_serveur , $utilisateur ,$mot_de_passe , $nom_base_données);
+        $req = mysqli_query($con , "SELECT * FROM users WHERE email = '$email' AND mdp ='$mdp' ") ;
+        $num_ligne = mysqli_num_rows($req) ;
+        if($num_ligne > 0){
+            header("Location:bienvenu.php") ;
+            $_SESSION['email'] = $email ;
+        }else {
+            $erreur = "Adresse Mail ou Mots de passe incorrectes !";
+        }
+    }
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulaire de connexion</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+   <section>
+       <h1> Connexion</h1>
+       <?php 
+       if(isset($erreur)){
+           echo "<p class= 'Erreur'>".$erreur."</p>"  ;
+       }
+       ?>
+       <form action="" method="POST"> 
+           <label>Adresse Mail</label>
+           <input type="text" name="email">
+           <label >Mots de Passe</label>
+           <input type="password" name="mdp">
+           <input type="submit" value="Valider" name="boutton-valider">
+       </form>
+   </section> 
+</body>
+</html>
