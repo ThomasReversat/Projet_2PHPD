6cfc633a1a30243e5be0c3eb7507b24c53e09d43
KWS0526 <yangv140@gmail.com>
2023-01-10 12:14:31 +0100

    e

--- accueil.php
+++ accueil.php
@@ -12,48 +12,48 @@
 
 </head>
 <body>
-<div class="header">
+<div class="container text-center">
     <h1>Bienvenue sur notre site</h1>
     <p>Nous sommes heureux de vous accueillir sur notre site web.</p>
+        <div class="research">
+            <form class="research-txt">
+                <input type="text" placeholder="Search...">
+                <button type="submit">Rechercher</button>
+            </form>
+            <select class="research-cat">
+                <option value="category1">Action</option>
+                <option value="category2">Drama</option>
+            </select>
+        </div>
+</div> 
+
+<div class="container text-center txt">
+    Voici les films disponibles actuellement sur notre site.
 </div>
 
-<div class="text">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Vestibulum consequat, purus a faucibus vestibulum, sapien.
-</div>
-
-<form class="research-bar">
-    <input type="text" placeholder="Search...">
-    <button type="submit">Go</button>
-</form>
-
-<select id="categories">
-  <option value="category1">Category 1</option>
-  <option value="category2">Category 2</option>
-</select>
 
 <div class="container text-center">
   <div class="row">
-    <div class="col">
-        <img src="img/topgun.jpg" alt="Movie Poster">
+    <div class="col col-margin">
+        <img src="img/topgun.jpg" class="img-fluid" alt="Movie Poster">
         <h3>Top Gun: Maverick</h3>
-        <p>Après plus de 30 ans de service en tant que l'un des meilleurs aviateurs de la Marine,
+        <p class="description">Après plus de 30 ans de service en tant que l'un des meilleurs aviateurs de la Marine,
             Pete Maverick Mitchell est à sa place, repoussant les limites en tant que pilote d'essai courageux et
             esquivant l'avancement de grade qui le mettrait à la terre.
         </p>
     </div>
-    <div class="col">
-        <img src="img/titanic.jpg" alt="Image 1">
+    <div class="col col-margin">
+        <img src="img/titanic.jpg" class="img-fluid" alt="Image 1">
         <h3>Titanic</h3>
-        <p>En 1997, l'épave du Titanic est l'objet d'une exploration fiévreuse, menée par des chercheurs de trésor
+        <p class="description">En 1997, l'épave du Titanic est l'objet d'une exploration fiévreuse, menée par des chercheurs de trésor
             en quête d'un diamant bleu qui se trouvait à bord. Frappée par un reportage télévisé, l'une des rescapées
             du naufrage, âgée de 102 ans, Rose DeWitt, se rend sur place et évoque ses souvenirs. 1912.
         </p>
     </div>
-    <div class="col">
-        <img src="img/avatar.jpg" alt="Movie Poster">
+    <div class="col col-margin">
+        <img src="img/avatar.jpg" class="img-fluid" alt="Movie Poster">
         <h3>Avatar: La voie de l'eau</h3>
-        <p>Jake Sully et Ney'tiri ont formé une famille et font tout pour rester aussi soudés que possible.
+        <p class="description">Jake Sully et Ney'tiri ont formé une famille et font tout pour rester aussi soudés que possible.
             Ils sont cependant contraints de quitter leur foyer et d'explorer les différentes régions encore mystérieuses
             de Pandora. Lorsqu'une ancienne menace refait surface, Jake va devoir mener une guerre difficile contre les
             humains.
