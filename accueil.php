423555424f4847b4d3c878fda48d71c3cb01be54
KWS0526 <yangv140@gmail.com>
2023-01-10 14:35:41 +0100

    debut de bdd

--- accueil.php
+++ accueil.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
 <head>
     <title>Internet Movies DataBase & co</title>
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="css/accueil.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <meta charset="utf-8">
-    <meta name="viewport">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="js/cat.js"></script>
 
 </head>
--- register.php
+++ register.php
@@ -1,163 +1,57 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
     <title>Internet Movies DataBase & co</title>
     <link rel="shortcut icon" type="image/ico" href="img/icone.ico">
-    <link rel="stylesheet" href="css/style.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <style>
-    /* Pour l'affichage sur les écrans de petite taille */
-    @media only screen and (max-width: 600px) {
-      .form-container {
-        width: 90%;
-        margin: 0 auto;
-        padding: 10px;
-        border-radius: 10px;
-        background-color: #f2f2f2;
-      }
-      .form-row {
-        width: 100%;
-        margin-bottom: 10px;
-      }
-      .form-label {
-        display: block;
-        width: 100%;
-        text-align: left;
-        font-weight: bold;
-        margin-bottom: 5px;
-      }
-      .form-input {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        box-sizing: border-box;
-        border: 2px solid #ccc;
-        border-radius: 4px;
-      }
-      .form-button {
-        width: 100%;
-        background-color: #4CAF50;
-        color: white;
-        padding: 14px 20px;
-        margin: 8px 0;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-      }
-      .form-button:hover {
-        background-color: #45a049;
-      }
-    }
-
-    /* Pour l'affichage sur les écrans de taille moyenne */
-    @media only screen and (min-width: 601px) and (max-width: 768px) {
-      .form-container {
-        width: 60%;
-        margin: 0 auto;
-        padding: 10px;
-        border-radius: 10px;
-        background-color: #f2f2f2;
-      }
-      .form-row {
-        width: 100%;
-        margin-bottom: 10px;
-      }
-      .form-label {
-        display: block;
-        width: 100%;
-        text-align: left;
-        font-weight: bold;
-        margin-bottom: 5px;
-      }
-      .form-input {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        box-sizing: border-box;
-        border: 2px solid #ccc;
-        border-radius: 4px;
-      }
-      .form-button {
-        width: 100%;
-        background-color: #4CAF50;
-        color: white;
-        padding: 14px 20px;
-        margin: 8px 0;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-      }
-      .form-button:hover {
-        background-color: #45a049;
-      }
-    }
-
-    /* Pour l'affichage sur les écrans de grande taille */
-    @media
-    only screen and (min-width: 769px) {
-  .form-container {
-    width: 40%;
-    margin: 0 auto;
-    padding: 10px;
-    border-radius: 10px;
-    background-color: #f2f2f2;
-  }
-  .form-row {
-    width: 100%;
-    margin-bottom: 10px;
-  }
-  .form-label {
-    display: block;
-    width: 100%;
-    text-align: left;
-    font-weight: bold;
-    margin-bottom: 5px;
-  }
-  .form-input {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    box-sizing: border-box;
-    border: 2px solid #ccc;
-    border-radius: 4px;
-  }
-  .form-button {
-    width: 100%;
-    background-color: #4CAF50;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-  }
-  .form-button:hover {
-    background-color: #45a049;
-  }
-}
-  </style>
+    <link rel="stylesheet" href="css/register.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body>
 
 <div class="form-container">
   <form action="/action_page.php">
-    <div class="form-row">
-      <label class="form-label" for="email">Email :</label>
-      <input type="text" class="form-input" id="email" name="email" required>
-    </div>
-    <div class="form-row">
-      <label class="form-label" for="pwd">Mot de passe :</label>
-      <input type="password" class="form-input" id="pwd" name="pwd" required>
-    </div>
-    <div class="form-row">
-      <label class="form-label" for="pwd2">Confirmer le mot de passe :</label>
-      <input type="password" class="form-input" id="pwd2" name="pwd2" required>
-    </div>
-    <div class="form-row">
-      <button type="submit" class="form-button">S'inscrire</button>
-    </div>
+    <fieldset>
+
+      <div class="form-row">
+        <label class="form-label" for="name">Nom :</label>
+        <input type="text" class="form-input" id="name" name="name" required>
+      </div>
+
+      <div class="form-row">
+        <label class="form-label" for="email">Prenom :</label>
+        <input type="text" class="form-input" id="first-name" name="first-name" required>
+      </div>
+
+      <div class="form-row">
+        <label class="form-label" for="email">Telephone :</label>
+        <input type="number" class="form-input" id="phone" name="phone" required>
+      </div>
+      
+      <div class="form-row">
+        <label class="form-label" for="email">Email :</label>
+        <input type="email" class="form-input" id="email" name="email" required>
+      </div>
+
+      <div class="form-row">
+        <label class="form-label" for="pwd">Mot de passe :</label>
+        <input type="password" class="form-input" id="pwd" name="pwd" required>
+      </div>
+
+      <div class="form-row">
+        <label class="form-label" for="pwd2">Confirmer le mot de passe :</label>
+        <input type="password" class="form-input" id="pwd2" name="pwd2" required>
+      </div>
+
+      <div class="form-row">
+        <button type="submit" class="form-button">S'inscrire</button>
+      </div>
+      
+    </fieldset>
   </form>
 </div>
 
 </body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 </html>
