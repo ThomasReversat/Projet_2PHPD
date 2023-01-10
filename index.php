b0154b46d28273b184a9bf3f0bdc72167cd187a3
KWS0526 <yangv140@gmail.com>
2023-01-10 10:07:39 +0100

    ajout bootstrap

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!doctype html>
-<html>
+<html lang="fr">
 <head>
     <title>Internet Movies DataBase & co</title>
     <link rel="shortcut icon" type="image/ico" href="img/icone.ico">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/default-style.css">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta name="viewport">
 </head>
 <body>
 <div class="header">
@@ -23,35 +25,35 @@
     <button type="submit">Go</button>
 </form>
 
-<div class="container">
-
-  <div class="image-block">
-    <img src="img/titanic.jpg" alt="Image 1">
-    <h3>Titanic</h3>
+<div class="container text-center">
+  <div class="row">
+    <div class="col">
+        <img src="img/topgun.jpg" alt="Movie Poster">
+        <h3>Top Gun: Maverick</h3>
+        <p>Après plus de 30 ans de service en tant que l'un des meilleurs aviateurs de la Marine,
+            Pete Maverick Mitchell est à sa place, repoussant les limites en tant que pilote d'essai courageux et
+            esquivant l'avancement de grade qui le mettrait à la terre.
+        </p>
+    </div>
+    <div class="col">
+        <img src="img/titanic.jpg" alt="Image 1">
+        <h3>Titanic</h3>
         <p>En 1997, l'épave du Titanic est l'objet d'une exploration fiévreuse, menée par des chercheurs de trésor
             en quête d'un diamant bleu qui se trouvait à bord. Frappée par un reportage télévisé, l'une des rescapées
-            du naufrage, âgée de 102 ans, Rose DeWitt, se rend sur place et évoque ses souvenirs. 1912.</p>
-  </div>
-
-  <div class="image-block">
-    <img src="img/avatar.jpg" alt="Movie Poster">
+            du naufrage, âgée de 102 ans, Rose DeWitt, se rend sur place et évoque ses souvenirs. 1912.
+        </p>
+    </div>
+    <div class="col">
+        <img src="img/avatar.jpg" alt="Movie Poster">
         <h3>Avatar: La voie de l'eau</h3>
         <p>Jake Sully et Ney'tiri ont formé une famille et font tout pour rester aussi soudés que possible.
             Ils sont cependant contraints de quitter leur foyer et d'explorer les différentes régions encore mystérieuses
             de Pandora. Lorsqu'une ancienne menace refait surface, Jake va devoir mener une guerre difficile contre les
             humains.
         </p>
-  </div>
-
-  <div class="image-block">
-    <img src="img/topgun.jpg" alt="Movie Poster">
-        <h3>Top Gun: Maverick</h3>
-        <p>Après plus de 30 ans de service en tant que l'un des meilleurs aviateurs de la Marine,
-            Pete Maverick Mitchell est à sa place, repoussant les limites en tant que pilote d'essai courageux et
-            esquivant l'avancement de grade qui le mettrait à la terre.</p>
-        </p>
+    </div>
   </div>
 </div>
-
 </body>
-</html>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script></html>
