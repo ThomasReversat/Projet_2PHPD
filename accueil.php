21011f974c96b1e98731179a26cc6a201efb8768
KWS0526 <yangv140@gmail.com>
2023-01-10 11:10:48 +0100

    d

--- accueil.php
+++ accueil.php
@@ -4,11 +4,11 @@
     <title>Internet Movies DataBase & co</title>
     <link rel="shortcut icon" type="image/ico" href="img/icone.ico">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/default-style.css">
+    <link rel="stylesheet" href="css/accueil.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <meta charset="utf-8">
     <meta name="viewport">
-    <script src="path/to/script.js"></script>
+    <script src="js/cat.js"></script>
 
 </head>
 <body>
@@ -63,4 +63,5 @@
 </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script></html>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+</html>
